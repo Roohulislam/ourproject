cf9b8b5b5313e561923e654d35c3cd9d761554dd
rohail jan <rohailjan726@gmail.com>
2025-05-26 19:31:31 +0500

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,23 @@
-import './App.css';
-import AboutUs from './components/AboutUs';
-import ContactUs from './components/ContactUs';
-import Footer from './components/Footer/Footer';
-import Location from './components/Location';
-import Navbar from './components/Navbar/Navbar';
-import Products from './components/Products/Products';
-import Services from './components/Services/Service';
-import Whatsapp from './components/Whatsapp';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import NotFound from './components/NotFound';
+import { allroute } from './Routes/allroute';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+import Whatsapp from './components/Whatsapp';
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="app-container">
+      <div className="min-h-screen flex flex-col">
         <Navbar />
-        <main className="main-content">
+        <main className="flex-grow">
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/services" element={<Services />} />
-            <Route path="/products" element={<Products />} />
-            <Route path="/locations" element={<Location />} />
-            <Route path="/aboutus" element={<AboutUs />} />
-            <Route path="/contactus" element={<ContactUs />} />
-            <Route path="*" element={<NotFound />} />
+            {allroute.map((route, index) => (
+              <Route key={index} path={route.path} element={route.element} />
+            ))}
           </Routes>
         </main>
-        <Whatsapp />
         <Footer />
+        <Whatsapp />
       </div>
     </BrowserRouter>
   );
