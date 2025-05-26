eef46cb97a286b924dc762d8ad0f4fe1dca7b7ff
rohail jan <rohailjan726@gmail.com>
2025-05-26 20:49:15 +0500

    buildddd

--- src/Routes/allroute.jsx
+++ src/Routes/allroute.jsx
@@ -4,7 +4,7 @@ import Services from "../components/Services/Service";
 import Location from "../components/Location";
 import AboutUs from "../components/AboutUs";
 import ContactUs from "../components/ContactUs";
-import NotFound from "../components/NotFound";
+// import NotFound from "../components/NotFound";
 
 export const allroute = [
   { path: "/", element: <Home /> },
@@ -13,5 +13,5 @@ export const allroute = [
   { path: "/locations", element: <Location /> },
   { path: "/aboutus", element: <AboutUs /> },
   { path: "/contactus", element: <ContactUs /> },
-  { path: "*", element: <NotFound /> } // Catch-all route
+  // { path: "*", element: <NotFound /> } // Catch-all route
 ];
